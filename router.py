48f14c14ccf577acee1a316c1698729f23bb3da0
aldian@workpc <aldian13777@gmail.com>
2026-04-10 14:46:52 +0800

    rev8 router

--- router.py
+++ router.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 def submit():
     name = request.form.get("name")
 
-    print(name)
+    return "data submitted"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
