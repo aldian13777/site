32950fafc7894917ffa5ebb29edcf1a98dafdcf6
aldian@workpc <aldian13777@gmail.com>
2026-04-10 09:12:36 +0800

    rev2

--- router.py
+++ router.py
@@ -2,10 +2,6 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    name = request.form['name']
-    return f"User: {name}"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/test')
+def test():
+    return "success"
