2d5fc4174e4ca4f62dce242622249f625fa20c9a
aldian@workpc <aldian13777@gmail.com>
2026-04-15 15:20:29 +0800

    revN dbtrial

--- router.py
+++ router.py
@@ -1,6 +1,15 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+#DB configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://neondb_owner:npg_z9PjKMr5dUWa@ep-restless-dust-aekvtftd.c-2.us-east-2.aws.neon.tech/site?sslmode=require&channel_binding=require"
+db = SQLAlchemy(app)
+
+#test DB connection
+@app.route('/connectiondb')
+def home():
+    return "Connected"
 
 # Collecting data from User input
 @app.route('/submit', methods=["POST"])
